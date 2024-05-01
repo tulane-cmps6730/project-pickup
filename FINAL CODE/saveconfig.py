034f48beecbe19fa305e87704ba037dfa8b67c08
mbookamer <79340162+mbookamer@users.noreply.github.com>
2024-04-30 23:05:24 -0500

    Delete FINAL CODE/saveconfig.py

--- FINAL CODE/saveconfig.py
+++
@@ -1,7 +0,0 @@
-from transformers import GPT2Config
-
-# Load the existing model's configuration
-model_config = GPT2Config.from_pretrained("./project-pickup-tuned")
-
-# Save the configuration to the same directory as your fine-tuned model
-model_config.save_pretrained("./project-pickup-tuned")
